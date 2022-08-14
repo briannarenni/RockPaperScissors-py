fda67fa26173f81c8c7db6261561e467cc832d6a
Brianna Renni <briannaxrene@icloud.com>
2022-08-13 17:52:28 -0700

    Update user input validation

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ import random
 
 # Computer's choice
 # Return a value of the options dictionary based on the random number generated
+
+
 def get_comp_choice():
     options = {1: 'Rock', 2: 'Paper', 3: 'Scissors'}
     rand_num = random.randint(1, 3)
@@ -11,12 +13,16 @@ def get_comp_choice():
     return comp_choice.lower()
 
 # User Choice
+
+
 def get_user_choice():
     user_input = input('Rock, paper, or scissors?')
     user_input = user_input.lower()
     return user_input
 
-#game result returns a str of 'win', 'lose', or 'draw'
+# game result returns a str of 'win', 'lose', or 'draw'
+
+
 def get_result(user_choice, comp_choice):
     # Rock beats scissors, scissors beat paper, paper beats rock
     if user_choice == comp_choice:
@@ -30,10 +36,15 @@ def get_result(user_choice, comp_choice):
     else:
         return f'You lose! {comp_choice} beats {user_choice}'
 
+
 # GAME LOGIC
 comp_choice = get_comp_choice()
 
-user_choice = get_user_choice()
+# get input until user enters 'rock', 'paper' or scissors
+while True:
+    user_choice = get_user_choice()
+    if user_choice in ['rock', 'paper', 'scissors']:
+        break
 
 result = get_result(user_choice, comp_choice)
 print(result)
